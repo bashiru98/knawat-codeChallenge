1da04e9e2b73ca28bb29e348ebe269616b778eca
bashiru98 <atulebashirubukari@gmail.com>
2021-08-04 03:05:04 +0300

    added auth service

--- services/products.service.ts
+++ services/products.service.ts
@@ -1,81 +1,81 @@
-"use strict";
+// "use strict";
 
-import { Service, ServiceBroker, ServiceSchema } from "moleculer";
+// import { Service, ServiceBroker, ServiceSchema } from "moleculer";
 
-import DbConnection from "../mixins/db.mixin";
-import CartActions from "../src/services/products/cartActions"
-export default class ProductsService extends Service{
+// import DbConnection from "../mixins/db.mixin";
+// import CartActions from "../src/services/products/cartActions"
+// export default class ProductsService extends Service{
 
-	private DbMixin = new DbConnection("products").start();
+// 	private DbMixin = new DbConnection("products").start();
 
-	// @ts-ignore
-	public  constructor(public broker: ServiceBroker, schema: ServiceSchema<{}> = {}) {
-		super(broker);
-		this.parseServiceSchema(Service.mergeSchemas({
-			name: "products",
-			mixins: [this.DbMixin],
-			settings: {
-				// Available fields in the responses
-				fields: [
-					"_id",
-					"name",
-					"quantity",
-					"price",
-				],
+// 	// @ts-ignore
+// 	public  constructor(public broker: ServiceBroker, schema: ServiceSchema<{}> = {}) {
+// 		super(broker);
+// 		this.parseServiceSchema(Service.mergeSchemas({
+// 			name: "products",
+// 			mixins: [this.DbMixin],
+// 			settings: {
+// 				// Available fields in the responses
+// 				fields: [
+// 					"_id",
+// 					"name",
+// 					"quantity",
+// 					"price",
+// 				],
 
-				// Validator for the `create` & `insert` actions.
-				entityValidator: {
-					name: "string|min:3",
-					price: "number|positive",
-					productId: "string",
-					qauntity: "number|positive",
+// 				// Validator for the `create` & `insert` actions.
+// 				entityValidator: {
+// 					name: "string|min:3",
+// 					price: "number|positive",
+// 					productId: "string",
+// 					qauntity: "number|positive",
 					
-				},
-			},
-			actions: {
-				addProductToCart: {
-					rest: "POST /:userId/cart",
-					params: {
-						userId: "string",
-						product: { type: "object" },
-					},
-					async handler(ctx) {
-						const entity = ctx.params.product
-						await this.validateEntity(entity)
-						const res = await new CartActions(ctx.params.userId, null).createCart(entity);
-					    await this.broker.cacher.clean(ctx.params.userId)
-						return {message:"Item added to cart",data:res}
-					 }
-				},
-				getCartSummary: {
-					rest: "GET /:userId/cart/summary",
-					params: {
-						userId: "string",
-					},
-					async handler(ctx) {
-						const cache = await this.broker.cacher.get(ctx.params.userId)
-						if (cache) {
+// 				},
+// 			},
+// 			actions: {
+// 				addProductToCart: {
+// 					rest: "POST /:userId/cart",
+// 					params: {
+// 						userId: "string",
+// 						product: { type: "object" },
+// 					},
+// 					async handler(ctx) {
+// 						const entity = ctx.params.product
+// 						await this.validateEntity(entity)
+// 						const res = await new CartActions(ctx.params.userId, null).createCart(entity);
+// 					    await this.broker.cacher.clean(ctx.params.userId)
+// 						return {message:"Item added to cart",data:res}
+// 					 }
+// 				},
+// 				getCartSummary: {
+// 					rest: "GET /:userId/cart/summary",
+// 					params: {
+// 						userId: "string",
+// 					},
+// 					async handler(ctx) {
+// 						const cache = await this.broker.cacher.get(ctx.params.userId)
+// 						if (cache) {
 							
-							// @ts-expect-error
-							return JSON.parse(cache)
-						}
-						const res =  await new CartActions(ctx.params.userId, null).getCartSummary();
-						await this.broker.cacher.set(ctx.params.userId, JSON.stringify(res))
+// 							// @ts-expect-error
+// 							return JSON.parse(cache)
+// 						}
+// 						const res =  await new CartActions(ctx.params.userId, null).getCartSummary();
+// 						await this.broker.cacher.set(ctx.params.userId, JSON.stringify(res))
 						
-						return res
-					 }
-				},
+// 						return res
+// 					 }
+// 				},
 				
-			},
-			methods: {
-				async seedDB() {
-					await this.adapter.insertMany([
-						{ name: "Samsung Galaxy S10 Plus", quantity: 10, price: 704 },
-						{ name: "iPhone 11 Pro", quantity: 25, price: 999 },
-						{ name: "Huawei P30 Pro", quantity: 15, price: 679 },
-					]);
-				},
-			},
-		}, schema));
-	}
-}
+// 			},
+// 			methods: {
+// 				async seedDB() {
+// 					await this.adapter.insertMany([
+// 						{ name: "Samsung Galaxy S10 Plus", quantity: 10, price: 704 },
+// 						{ name: "iPhone 11 Pro", quantity: 25, price: 999 },
+// 						{ name: "Huawei P30 Pro", quantity: 15, price: 679 },
+// 					]);
+// 				},
+// 			},
+// 		}, schema));
+// 	}
+// }
