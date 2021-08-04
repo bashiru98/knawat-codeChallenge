d1b24161be7da935b180ec331bbba8005c1b1b7a
bashiru98 <atulebashirubukari@gmail.com>
2021-08-04 04:06:17 +0300

    added user jwt operations

---
+++ src/utils/types.ts
@@ -0,0 +1,14 @@
+"use strict";
+
+export interface UserPayload {
+    username: string,
+    id:string,
+    email: string,
+    createdAt:string,
+    exp: Date,
+}
+
+export interface Product {
+
+}
+
